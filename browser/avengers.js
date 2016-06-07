91af6ba5fb0adcca7c37f381df51d21d9792659b
Emma Bishop <emma.b.bishop@gmail.com>
2016-06-07 04:18:24 -0400

    Aggregated by gender, grouped by year, with axes

--- browser/avengers.js
+++ browser/avengers.js
@@ -1,71 +1,138 @@
-var height = 500;
+var margin = {top: 20, right: 30, bottom: 30, left: 20};
+
+// set width & height of chart
+var height = 500 - margin.top - margin.bottom;
+var width = 1050 - margin.left - margin.right;
+
 var barWidth = 20;
 
-var y = d3.scale.linear().range([0, height]);
+// scale for x-axis
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width]);
 
+// scale for y-axis
+var y = d3.scale.linear()
+    .range([0, height]);
+
+
+// select chart element, set width, height & margins
 var chart = d3.select('.bar-chart')
-              .attr('height', height);
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-d3.csv('avengers-txt.csv', function (err, rows) {
 
+// get data
+d3.csv('avengers-txt.csv', function(err, rows){
   // group data by year and gender
-  var nestedData = d3.nest()
-      .key(function(d) { return d.Year; })
-      .sortKeys(d3.ascending)
-      .rollup(function(d){
-        return {
-          maleCt: d.filter(function(r){
-            return r.Gender === 'MALE';
-          }).length,
-          femaleCt: d.filter(function(r){
-            return r.Gender === 'FEMALE';
-          }).length
-        };
-      })
-      .entries(rows);
+  var groupedByYear = d3.nest()
+        .key(function(d) {
+          return d.Year;
+        })
+        .sortKeys(d3.ascending)
+        .entries(rows);
 
-      console.log(nestedData);
+  var parsedData = parseGenderData(groupedByYear);
 
-  // get maximum count per gender per year
-  var maxCt = d3.max(nestedData, function(y){
-    return y.values.maleCt > y.values.femaleCt ? y.values.maleCt : y.values.femaleCt;
-  });
+  // set scale for x-axis
+   x.domain(parsedData.map(function(d){
+      return d.year;
+    }));
 
-  // scale to use for y-axis
-  y.domain([0, maxCt]);
+  // get maximum count for any gender, in any year
+  var maxCt = d3.max(parsedData, getMaxPerYear);
 
-  // width of svg, based on size of dataset
-  chart.attr('width', barWidth * 2 * nestedData.length);
+  // set scale for y-axis
+  y.domain([maxCt, 0]);
 
-  var year = chart.selectAll('g')
-            .data(nestedData)
-            .enter().append('g')
-            .attr('transform', function(d, i){
-              return 'translate(' + i * barWidth * 2 + ', 0)';
-            })
-            .attr('height', height);
+  // x-axis generator
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient('bottom');
 
-  // append female data
-  year.append('rect')
-      .attr('height', function(d){
-        return y(d.values.femaleCt);
-      })
-      .attr('width', barWidth - 1)
-      .attr('class', 'female')
-      .attr('transform', function(d, i) {
-        return 'translate(0, ' + (height - y(d.values.femaleCt)) + ')';
-      });
+  // y-axis generator
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient('left');
 
-  // append male
-  year.append('rect')
-      .attr('height', function(d){
-        return y(d.values.maleCt);
-      })
-      .attr('width', barWidth - 1)
-      .attr('class', 'male')
+  // append x-axis
+  chart.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0, ' + height + ')')
+    .call(xAxis);
+
+  chart.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis);
+
+
+  var year = chart.selectAll('.year')
+      .data(parsedData)
+      .enter()
+      .append('g')
+      .attr('class', 'year')
       .attr('transform', function(d, i){
-        return 'translate(' + (barWidth - 1) + ', ' + (height - y(d.values.maleCt)) + ')';
-      });
+        return 'translate(' + (i * barWidth * 2 ) + ', 0)';
+      })
+      .attr('height', height)
+      .attr('width', barWidth);
+
+      // add bars to years
+    year.selectAll('.bar')
+      .data(function(d, i) {
+        return d.data;
+      })
+      .enter()
+      .append('rect')
+      .attr('class', function(d){
+        return 'bar ' + d.gender;
+      })
+      .attr('width', barWidth -1)
+      .attr('height', function(d) {
+        return height - y(d.values.length);
+      })
+      .attr('transform', getTransformation);
 
 
 });
+
+function getMaxPerYear(year) {
+  return d3.max(year.data, function(d){
+    return d.values.length;
+  });
+}
+
+function parseGenderData(rows) {
+  var data = [], temp;
+
+  for (var i = 0, len = rows.length; i < len; i ++){
+    temp = {};
+    temp.year = rows[i].key;
+    temp.data = [
+      {
+        gender: 'female',
+        values: rows[i].values.filter(function(a){
+          return a.Gender === 'FEMALE';
+        })
+      },
+      {
+        gender: 'male',
+        values: rows[i].values.filter(function (a) {
+          return a.Gender === 'MALE';
+        })
+      }
+    ];
+    data.push(temp);
+  }
+
+  return data;
+}
+
+function getTransformation (d) {
+  var horizontal = 0;
+  if (d.gender === 'male') {
+    horizontal += barWidth - 1;
+  }
+  return 'translate(' + horizontal + ', ' + (height - (height - y(d.values.length)) )+ ')';
+}
