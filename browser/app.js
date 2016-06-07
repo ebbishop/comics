6b614b09940fadb2b147676490f68b8ac45cf073
Emma Bishop <emma.b.bishop@gmail.com>
2016-06-06 22:45:46 -0400

    Rotates chart to vertical bars

--- browser/app.js
+++ browser/app.js
@@ -1,34 +1,33 @@
-// var data = [4, 8, 15, 16, 23, 0, 42];
+var height = 500,
+    barWidth = 20;
 
-var width = 420,
-    barHeight = 20;
+var y = d3.scale.linear()
+        .range([0, height]);
 
-var x = d3.scale.linear()
-        .range([0, width]);
-
-// set width attribute of entire svg element
+// set height attribute of entire svg element
 var chart = d3.select('.bar-chart')
-            .attr('width', width)
+            .attr('height', height);
 
 d3.csv('avengers-txt.csv', type, function(err, data){
-  x.domain([0, d3.max(data, function(d){ return d.Appearances })]);
-  chart.attr('height', barHeight * data.length);
+  y.domain([0, d3.max(data, function(d){ return d.Appearances; })]);
+
+  // width of svg based on size of dataset
+  chart.attr('width', barWidth * data.length);
 
   // create g element for each datum
   var bar = chart.selectAll('g')
           .data(data)
           .enter().append('g')
-          .attr('transform', function(d, i) { return 'translate(0,' + i * barHeight + ')'}); //horizontal, vertical translation
+          .attr('transform', function(d, i) { return 'translate(' + i * barWidth + ', ' + (height - y(d.Appearances)) + ')'; }); //horizontal, vertical translation
 
   bar.append('rect')
-    .attr('width', function(d){return x(d.Appearances)})
-    .attr('height', barHeight - 1); // -1 gives space between bars vertically
+    .attr('height', function(d){return y(d.Appearances); })
+    .attr('width', barWidth - 1); // -1 gives space between bars vertically
 
   bar.append('text')
-    .attr('x', function(d){ return x(d.Appearances) - 3;}) // x relative to parent element
-    .attr('y', barHeight/2)
-    .attr('dy', '.35em')
-    .text(function (d) { return d['Name/Alias'] + ' ' + d.Appearances });
+    .text(function (d) { return d.Appearances; })
+    .attr('transform', function(d, i) { return 'translate(' + barWidth/4 + ',' + 3 + ')rotate(-90)'; })
+    .attr('dy', '.75em');
 });
 
 function type(d) {
