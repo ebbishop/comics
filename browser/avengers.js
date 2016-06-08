b5edc7244215a97d028654036cf630e03598edc1
Emma Bishop <emma.b.bishop@gmail.com>
2016-06-08 14:14:26 -0400

    re-organizes files, adds gridlines

--- browser/avengers.js
+++ browser/avengers.js
@@ -1,54 +1,5 @@
 'use strict';
 
-// --------------------
-// SET UP FOR BAR CHART
-// --------------------
-
-  var margin = {top: 20, right: 30, bottom: 30, left: 30};
-
-  // set width & height of chart
-  var height = 500 - margin.top - margin.bottom;
-  var width = 1150 - margin.left - margin.right;
-
-
-  // scale for x-axis
-  var x = d3.scale.ordinal()
-      .rangeRoundBands([0, width]);
-
-  // scale for y-axis
-  var y = d3.scale.linear()
-      .range([0, height]);
-
-
-  // select chart element, set width, height & margins
-  var chart = d3.select('.bar-chart')
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-      .append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-// ---------------------
-// SET UP FOR DATA TABLE
-// ---------------------
-
-  var headers = ['Name/Alias', 'Gender', 'Year', 'Notes'];
-
-  // initialize table with headers
-  var table = d3.select('table');
-
-  table.append('thead')
-    .selectAll('th')
-    .data(headers)
-    .enter().append('th')
-    .html(function(d){
-      return d;
-    });
-
-  var tableBody = table.append('tbody');
-
-
-
-
 
 // --------
 // GET DATA
@@ -81,13 +32,6 @@ function padString(str, max, padWith){
   return str;
 }
 
-function abbreviate(str){
-  return str.split('/')[0].toLowerCase();
-}
 
-function extractName(url){
-  var name = url.split('/')[url.split('/').length-1];
-  // find the first non-word character and trim away following
-  var firstIdx = name.search(/\W/i);
-  return name.slice(0, firstIdx).replace(/[^a-z]/ig, ' ').replace(/$\s/,'');
-}
+
+
--- browser/barchart.js
+++ browser/barchart.js
@@ -1,68 +1,88 @@
 'use strict';
 
 // --------------------
+// SET UP FOR BAR CHART
+// --------------------
+
+  var margin = {top: 20, right: 30, bottom: 30, left: 30};
+
+  // set width & height of chart
+  var height = 500 - margin.top - margin.bottom;
+  var width = 1150 - margin.left - margin.right;
+
+
+  // pixel scale for x-axis
+  var x = d3.scale.ordinal()
+      .rangeRoundBands([0, width], 0.1);
+
+  // pixel scale for y-axis
+  var y = d3.scale.linear()
+      .range([0, height]);
+
+  // select chart element, set width, height & margins
+  var chart = d3.select('.bar-chart')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+// --------------------
 // DRAW BAR CHART
 // --------------------
 
 function createBarChart(rows){
   // group data by year
-  var groupedByYear = d3.nest()
-        .key(function(d) {
-          return d.Year;
-        })
-        .sortKeys(d3.ascending)
-        .entries(rows);
-
+  var groupedByYear = groupByYear(rows);
   var parsedData = parseGenderData(groupedByYear);
 
 
-  // set scale for x-axis
+  // set ordinals for x-axis
    x.domain(parsedData.map(function(d){
       return d.year;
     }));
 
   var barWidth = x.rangeBand()/2;
-  console.log('barWidth', barWidth)
 
   // get maximum count for any gender, in any year
   var maxCt = d3.max(parsedData, getMaxPerYear);
 
-  // set scale for y-axis
+  // set value scale for y-axis
   y.domain([maxCt, 0]);
 
-  // x-axis generator
-  var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient('bottom');
 
-  // y-axis generator
-  var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient('left');
+  // append y-axis
+  chart.append('g')
+    .attr('class', 'y axis')
+    .call(makeYAxis());
+
+  // append y-axis gridlines
+  chart.append('g')
+    .attr('class', 'grid')
+    .call(makeYAxis()
+      .tickSize(-width, 0, 0)
+      .tickFormat(''))
+    .attr('transform', 'translate(.5, 0)');
 
   // append x-axis
   chart.append('g')
     .attr('class', 'x axis')
     .attr('transform', 'translate(0, ' + height + ')')
-    .call(xAxis);
-
-  chart.append('g')
-    .attr('class', 'y axis')
-    .call(yAxis);
+    .call(makeXAxis());
 
 
+  // insert groups for each year's data
   var year = chart.selectAll('.year')
       .data(parsedData)
       .enter()
       .append('g')
       .attr('class', 'year')
       .attr('transform', function(d, i){
-        return 'translate(' + (i * barWidth * 2 ) + ', 0)';
+        return 'translate(' + (x(d.year) ) + ', 0)';
       })
       .attr('height', height)
       .attr('width', barWidth * 2);
 
-      // add bars to years
+    // add two bars to each year's group
     year.selectAll('.bar')
       .data(function(d, i) {
         return d.data;
@@ -72,7 +92,7 @@ function createBarChart(rows){
       .attr('class', function(d){
         return 'bar ' + d.gender;
       })
-      .attr('width', barWidth - 1)
+      .attr('width', barWidth)
       .attr('height', function(d) {
         return height - y(d.values.length);
       })
@@ -80,16 +100,38 @@ function createBarChart(rows){
       .on('click', toggleSelectedBar);
 }
 
+
+
+
 // -----------------
 // BAR CHART HELPERS
 // -----------------
+
+function makeXAxis() {
+  return d3.svg.axis()
+    .scale(x)
+    .orient('bottom');
+}
+
+function makeYAxis(){
+  return d3.svg.axis()
+    .scale(y)
+    .orient('left');
+}
 
 function getMaxPerYear(year) {
   return d3.max(year.data, function(d){
     return d.values.length;
   });
 }
-
+function groupByYear(rows) {
+  return d3.nest()
+    .key(function(d) {
+      return d.Year;
+    })
+    .sortKeys(d3.ascending)
+    .entries(rows);
+}
 // re-format data nesting for better access to data counts & details
 function parseGenderData(rows) {
   var data = [], temp;
@@ -118,12 +160,11 @@ function parseGenderData(rows) {
 }
 
 function getTransformation(d, i) {
-  console.log(x.rangeBand()/2, i);
-  var horizontal = x.rangeBand()/4 - 1.5;
+  var horizontal = 0;
   if (d.gender === 'male') {
-    horizontal += (x.rangeBand()/2) - 1;
+    horizontal += x.rangeBand()/2;
   }
-  return 'translate(' + horizontal + ', ' + (height - (height - y(d.values.length)) -.5 )+ ')';
+  return 'translate(' + horizontal + ', ' + (height - (height - y(d.values.length)) - .5)+ ')';
 }
 
 
